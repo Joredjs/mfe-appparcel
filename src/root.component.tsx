98fa60b7e4118a406cf3dcfbe447c1640b2c5777
joredjs <odraude160@gmail.com>
2023-01-04 14:15:31 -0500

    test (prettier): Agregar archivo prettierrc para que coincida con el lint

---
+++ src/root.component.tsx
@@ -0,0 +1,10 @@
+export default function Root(props) {
+  //console.log(props)
+  //return <section>{props.name} is mounted!</section>
+  return (
+    <>
+      <header>{props.name} is mounted!</header>
+      <section>Bienvenido</section>
+    </>
+  );
+}
